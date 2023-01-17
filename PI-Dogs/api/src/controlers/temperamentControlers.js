421f0b2db53d689e44074262d42ad546febb64c7
JavGarb <olguiyjavi@gmail.com>
2023-01-16 22:54:07 -0500

    back terminado se agregaron comentarios

--- PI-Dogs/api/src/controlers/temperamentControlers.js
+++ PI-Dogs/api/src/controlers/temperamentControlers.js
@@ -5,16 +5,22 @@ const { getAll } = require('../controlers/dogControlers');
 //funcion para tomar todos los 
 function allTemperamentos(arr) {
   let temporal = [];
-  let resultado=[];
+  let resultado=[];// la usare para entregar los resultados
+  //recorro todo el array para sacar los temperamentos
   arr.forEach((elemento) =>{
-    let{temperamento}= elemento;
+    let{temperamento}= elemento;//desestructuro el temperamento
+    //tengo que verificar que el temperamento contenga algo
     if(temperamento){
+      //temperamento es un string separado por comas, por tanto
+      //hago un split que me devuelve un array con las palabras
       temporal= temperamento.split(',');
+      //recorro el array y vou pusheando al resultado
       temporal.map(elem=>{
         resultado.push(elem.trim())
       })
     }
   });
+  //retorno el resultado que es un array con los temperamentos incluye los repetidos
   return resultado;
 }
 
@@ -26,7 +32,7 @@ async function temperamentosADB() {
   let arr = await getAll();//trae todos los datos de la api
   //me trae una lista con todos los temperamentos enviandole todos los datos
   let temp = allTemperamentos(arr);
-  //busca si ya existe el temperamento sino lo crea
+  //busca si ya existe el temperamento en la base de datos postgres sino lo crea
   temp.forEach(async (element) => {
     const [createdElement, created] = await Temperamento.findOrCreate({
       where: { name:element },
